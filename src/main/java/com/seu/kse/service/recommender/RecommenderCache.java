1cb288a8ac82da90a8bd70f5f8ff2e43d429198e
yaosheng <877717444@@qq.com>
2017-12-17 19:53:24 +0800

    Merge branch 'master' of https://github.com/yaosheng42/AIOnlineRecommedation
    # Conflicts:
    #	src/main/java/com/seu/kse/listener/DataInjectContextListener.java
    #	src/main/java/com/seu/kse/listener/OffLineModelListener.java
    #	src/main/java/com/seu/kse/service/DataInjectService.java

--- src/main/java/com/seu/kse/service/recommender/RecommenderCache.java
+++ src/main/java/com/seu/kse/service/recommender/RecommenderCache.java
@@ -1,14 +1,29 @@
 package com.seu.kse.service.recommender;
 
+import org.deeplearning4j.models.word2vec.Word2Vec;
+import org.nd4j.linalg.api.ndarray.INDArray;
+
+import java.util.List;
+
+
 /**
  * 存储推荐过程中需要的对象(皆为静态唯一的对象)
  */
 public class RecommenderCache {
     //论文相似度矩阵
+    public static INDArray paperSimilarityMatrix ;
 
-    //论文向量矩阵
+    //论文向量矩阵 row : paper's num ; col : dimension
+    public static INDArray paperVectorMatrix;
+
+    //word2vec model
+    public static Word2Vec word2vec;
 
 
+    //row id 和 用户 ID映射表
+    public static List<String> userRowMapID;
 
+    //row id 和 论文 ID映射表
+    public static List<String> paperRowMapID;
 
 }
